4e00d4871c2db87614870d25243f2b9ddf3bbad7
svara12 <105842062+svara12@users.noreply.github.com>
2022-05-19 12:26:41 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>welcome to online techinal forum</title>
+  </head>
+  <style>
+  body, html {
+  height: 100%;
+  font-family: "Inconsolata", sans-serif;
+}
+
+.bgimg {
+  background-position: center;
+  background-size: cover;
+  background-image: url("https://media.istockphoto.com/photos/black-carbon-fiber-motion-background-technology-wavy-line-with-red-picture-id1371966433?b=1&k=20&m=1371966433&s=170667a&w=0&h=vVtq7TA_9fuJfemAsWv1mkR7oXmt7sZz6AId6Apy53Q=");
+  min-height: 75%;
+}
+
+.menu {
+  display: none;
+}
+  </style>
+  <body>
+  <?php include 'dbconnect.php';?>
+  <?php include 'navbar.php';?>
+  <header class="bgimg w3-display-container w3-grayscale-min" id="home">
+  <div class="w3-display-bottomleft w3-center w3-padding-large w3-hide-small">
+    <span class="w3-tag">ask share</span>
+  </div>
+  <div class="w3-display-middle w3-center">
+    <span class="w3-text-white" style="font-size:90px">online techinal forum</span>
+  </div>
+  
+</header>
+
+<!-- Add a background color and large text to the whole page -->
+<div class="w3-sand w3-grayscale w3-large">
+
+<!-- About Container -->
+<div class="w3-container" id="about">
+  <div class="w3-content" style="max-width:700px">
+    <h5 class="w3-center w3-padding-64"><span class="w3-tag w3-wide">ABOUT THE welcome to online techinal forum</span></h5>
+    <p>An Internet forum, or message board, is an online discussion site where people can hold conversations in the form of posted messages. They differ from chat rooms in that messages are often longer than one line of text, and are at least temporarily archived.</p>
+    <p>An Internet forum, or message board, is an online discussion site where people can hold conversations in the form of posted messages. They differ from chat rooms in that messages are often longer than one line of text, and are at least temporarily archived.</p>
+    <div class="w3-panel w3-leftbar w3-light-grey">
+      
+    </div>
+    <img src="https://media.istockphoto.com/photos/black-carbon-fiber-motion-background-technology-wavy-line-with-red-picture-id1371966433?b=1&k=20&m=1371966433&s=170667a&w=0&h=vVtq7TA_9fuJfemAsWv1mkR7oXmt7sZz6AId6Apy53Q=" style="width:100%;max-width:1000px" class="w3-margin-top">
+    
+  </div>
+</div>
+
+  <?php include 'partials/_footer.php';?> 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
